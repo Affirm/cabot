a6dbd616cbd5f4e34be4aa62424640fd7c2a6d3d
Alec Lofquist <alec.lofquist@affirm.com>
2018-12-10 17:57:28 -0800

    Add create ack button

--- cabot/cabotapp/views.py
+++ cabot/cabotapp/views.py
@@ -1020,7 +1020,7 @@ class AckCreateView(LoginRequiredMixin, CreateView):
             check = None
 
         return {
-            'status_check': check.pk,
+            'status_check': check.pk if check else '',
             'tags': result.tags.all() if result else None,
             'match_if': Acknowledgement.MATCH_CHECK if not result else Acknowledgement.MATCH_ALL_IN,
             'expire_at': self.request.GET.get('expire_after_hours', '4')  # default to expiring after 4 hours
