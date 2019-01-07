ef4762ef80d3d632854a81c07e60ceac1150e5c7
Alec Lofquist <alces14@gmail.com>
2019-01-07 10:10:55 -0800

    Fix unicode series name parsing + add tests (#159)

--- cabot/metricsapp/models/elastic.py
+++ cabot/metricsapp/models/elastic.py
@@ -10,6 +10,7 @@ from elasticsearch_dsl import MultiSearch, Search
 from cabot.metricsapp.api import create_es_client, validate_query
 from cabot.metricsapp import defs
 from .base import MetricsSourceBase, MetricsStatusCheckBase
+import six
 
 
 logger = logging.getLogger(__name__)
@@ -145,7 +146,7 @@ class ElasticsearchStatusCheck(MetricsStatusCheckBase):
         except Exception as e:
             logger.exception('Error executing Elasticsearch query: {}'.format(query))
             parsed_data['error_code'] = type(e).__name__
-            parsed_data['error_message'] = str(e)
+            parsed_data['error_message'] = six.text_type(e)
             parsed_data['error'] = True
 
         return parsed_data
@@ -216,7 +217,7 @@ class ElasticsearchStatusCheck(MetricsStatusCheckBase):
 
         for subseries in series:
             if isinstance(subseries, tuple):
-                series_name = '.'.join(filter(None, [original_series_name, str(subseries[0])]))
+                series_name = u'.'.join(filter(None, [original_series_name, six.text_type(subseries[0])]))
                 subseries = subseries[1]
 
             if subseries.get('agg') is None:
@@ -226,8 +227,8 @@ class ElasticsearchStatusCheck(MetricsStatusCheckBase):
                 # New name is "series_name.subseries_name" (if they exist)
                 key = subseries.get('key')
                 if key is not None:
-                    key = str(key)
-                subseries_name = '.'.join(filter(None, [series_name, key]))
+                    key = six.text_type(key)
+                subseries_name = u'.'.join(filter(None, [series_name, key]))
                 results = self._parse_series(subseries['agg']['buckets'], series_name=subseries_name)
 
             for result in results:
